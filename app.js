b25ae00907649608e4fc41aed51523ca1e4a5977
Thomas <tthomas6@una.edu>
2024-08-29 13:59:27 -0500

    saving

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const app = express()
 
+// app.use(express.static('./'))
+
+const path = require('path')
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
 app.get('/', function (req, res) {
   res.send('Hello World')
+})
+
+app.get('/static', (req,res)=> {
+
+  res.sendFile('index.html')
 })
 
 app.listen(3000)
